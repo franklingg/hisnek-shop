e863eb60efeee16efa3eb74ff7f05292709046d2
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2024-03-22 19:45:35 -0300

    feat: add graphql serverfefe

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv'],
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+  ],
 };
