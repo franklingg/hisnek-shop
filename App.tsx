b8622bdf6d8d93199b26af2914692b4401619940
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2024-03-24 12:05:39 -0300

    feat: add cart context and its methods

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 
 import Routes from './src/routes';
+import {CartProvider} from '~/contexts/cartContext';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <CartProvider>
+      <Routes />
+    </CartProvider>
+  );
 };
 
 export default App;
---
+++ src/contexts/cartContext.tsx
@@ -0,0 +1,80 @@
+import React, {createContext, useState, useContext, useCallback} from 'react';
+import {CartProduct} from '~/services/types';
+
+interface ICartContext {
+  cartItems: Array<CartProduct>;
+  addItem: (cartItem: CartProduct) => void;
+  removeItem: (productId: number) => void;
+  clearCart: () => void;
+  changeQuantity: (productId: number, newQuantity: number) => void;
+}
+
+const defaultState: ICartContext = {
+  cartItems: [],
+  addItem: () => {},
+  removeItem: () => {},
+  clearCart: () => {},
+  changeQuantity: () => {},
+};
+
+const CartContext = createContext<ICartContext>(defaultState);
+
+export function CartProvider({children}: {children: React.ReactNode}) {
+  const [cartItems, setCartItems] = useState(defaultState.cartItems);
+
+  const removeItem = useCallback(
+    (productId: number) => {
+      setCartItems([...cartItems.filter(i => i.product.id !== productId)]);
+    },
+    [cartItems],
+  );
+
+  const changeQuantity = useCallback(
+    (productId: number, newQuantity: number) => {
+      const items = [...cartItems];
+      const idxToChange = items.findIndex(i => i.product.id === productId);
+      if (idxToChange !== -1) {
+        if (newQuantity > 0) {
+          items[idxToChange].quantity = newQuantity;
+          setCartItems(items);
+        } else {
+          removeItem(productId);
+        }
+      }
+    },
+    [cartItems, removeItem],
+  );
+
+  const addItem = useCallback(
+    (newItem: CartProduct) => {
+      const existingItemIdx = cartItems.findIndex(
+        i => i.product.id === newItem.product.id,
+      );
+      if (existingItemIdx !== -1) {
+        changeQuantity(
+          newItem.product.id,
+          cartItems[existingItemIdx].quantity + newItem.quantity,
+        );
+      } else {
+        setCartItems([...cartItems, newItem]);
+      }
+    },
+    [cartItems, changeQuantity],
+  );
+
+  const clearCart = useCallback(() => {
+    setCartItems([]);
+  }, []);
+
+  return (
+    <CartContext.Provider
+      value={{cartItems, addItem, removeItem, clearCart, changeQuantity}}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  return context;
+}
